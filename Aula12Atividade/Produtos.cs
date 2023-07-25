82492c155e5b48cbbc2ae65178c91e134562abaf
Victor <135838623+Vict0r-Santos@users.noreply.github.com>
2023-07-25 17:20:34 -0300

    Add files via upload

---
+++ Aula12Atividade/Produtos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Atividade
+{
+    internal class Produto
+    {
+        public string nome;
+        public string descricao;
+        public double preço;
+        public int quantidade;
+        public bool status;
+
+        public void mostrar()
+        {
+
+            Console.WriteLine($"Produto: {nome}\nDescrição: {descricao}\nPreço: {preço}\nQuantidade: {quantidade}\n Status: {status}\n");
+
+
+        }
+
+        public void desativar()
+        {
+            string escolha;
+            Console.WriteLine("\nDesativar produto? S/N\n");
+            escolha = Console.ReadLine();
+            if ( escolha == "S" )
+            {
+                status = false;
+                quantidade = 0;
+            }
+            else
+            {
+                status = true;
+            }
+        }
+    }
+}
---
+++ Aula12Atividade/Program.cs
@@ -0,0 +1,39 @@
+﻿using Aula12Atividade;
+
+Console.WriteLine("Hello, World!");
+
+Produto caixa = new Produto();
+
+caixa.nome = "Caixa";
+caixa.preço = 10.00;
+caixa.quantidade = 10;
+caixa.descricao = "É uma caixa.";
+caixa.status = true;
+
+caixa.mostrar();
+caixa.desativar();
+
+
+Produto celular = new Produto();
+celular.nome = "Iphone 666";
+celular.preço = 6.666;
+celular.quantidade = 666;
+celular.descricao = "Vindo diretamente das profundezas do inferno, o Iphone 666 promete entregar o pior desempenho possível";
+celular.status = true;
+
+celular.mostrar();
+celular.desativar();
+
+Produto carro = new Produto();
+carro.nome = "Fiat Dos";
+carro.preço = 50.000;
+carro.quantidade = 1;
+carro.descricao = "É tipo o Fiat uno só que pior";
+carro.status= true;
+
+carro.mostrar();
+carro.desativar();
+
+caixa.mostrar();
+celular.mostrar();
+carro.mostrar();
